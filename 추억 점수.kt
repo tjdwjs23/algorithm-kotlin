9caf0c8314104a77e14ba908440061a3db4df031
SeonghunChoi <77057201+tjdwjs23@users.noreply.github.com>
2024-03-11 22:45:05 +0900

    Update 추억 점수.kt

--- 추억 점수.kt
+++ 추억 점수.kt
@@ -4,8 +4,7 @@
  * 다른 사진 속 인물의 이름이 ["kali", "mari", "don", "tony"]이고 ["kali", "mari", "don"]의 그리움 점수가 각각 [11점, 1점, 55점]]이고, "tony"는 그리움 점수가 없을 때, 이 사진의 추억 점수는 3명의 그리움 점수를 합한 67(11 + 1 + 55)점입니다.
  * 그리워하는 사람의 이름을 담은 문자열 배열 name, 각 사람별 그리움 점수를 담은 정수 배열 yearning, 각 사진에 찍힌 인물의 이름을 담은 이차원 문자열 배열 photo가 매개변수로 주어질 때, 
  * 사진들의 추억 점수를 photo에 주어진 순서대로 배열에 담아 return하는 solution 함수를 완성해주세요.
- **
-/
+ */
 
 class Solution {
     fun solution(name: Array<String>, yearning: IntArray, photo: Array<Array<String>>): List<Int> {
