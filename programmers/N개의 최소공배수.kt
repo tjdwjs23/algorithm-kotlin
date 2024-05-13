a3d036a66d35061be0c02478f2d18f77434f75fd
SeonghunChoi <77057201+tjdwjs23@users.noreply.github.com>
2024-05-13 09:42:15 +0900

    Update N개의 최소공배수.kt

--- programmers/N개의 최소공배수.kt
+++ programmers/N개의 최소공배수.kt
@@ -4,21 +4,23 @@
  */
 
 class Solution {
+    // fold 함수를 이용하여 배열의 모든 요소에 대해 최소공배수를 구하는 solution 함수 정의
     fun solution(arr: IntArray): Int = arr.fold(1) {lcm, i -> getLcm(lcm, i) }
 
-    // 최대공약수를 구하는 함수
+    // 두 정수의 최대공약수를 구하는 함수, 유클리드 호제법을 사용
     private fun getGcd(targetNumber1 : Int, targetNumber2 : Int) : Int {
-        var gcm = targetNumber1
-        var divisor = targetNumber2
-        var rest = 0
-        while(divisor != 0) {
-            rest = gcm % divisor
-            gcm = divisor
-            divisor = rest
+        var gcm = targetNumber1 // 임시 최대공약수 변수 초기화
+        var divisor = targetNumber2 // 나누는 수 초기화
+        var rest = 0 // 나머지를 저장할 변수 초기화
+        while(divisor != 0) { // 나누는 수가 0이 아닐 때까지 반복
+            rest = gcm % divisor // 현재 나누는 수로 나눈 나머지 계산
+            gcm = divisor // 나누는 수를 최대공약수로 업데이트
+            divisor = rest // 나머지를 새로운 나누는 수로 설정
         }
-        return gcm
+        return gcm // 계산된 최대공약수 반환
     }
 
-    // 유클리드 호제법을 이용하여 최소공배수를 구하는 함수
-    private fun getLcm(targetNumber1: Int, targetNumber2: Int) : Int = targetNumber1 * targetNumber2 / getGcd(targetNumber1, targetNumber2)
+    // 두 정수의 최소공배수를 구하는 함수, 유클리드 호제법의 최대공약수를 이용
+    private fun getLcm(targetNumber1: Int, targetNumber2: Int) : Int =
+        targetNumber1 * targetNumber2 / getGcd(targetNumber1, targetNumber2) // 두 수의 곱을 최대공약수로 나눈 값이 최소공배수
 }
