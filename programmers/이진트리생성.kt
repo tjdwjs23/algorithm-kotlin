600fb1c28bb1591efe812c4dc42137ba6bb10265
SeonghunChoi <77057201+tjdwjs23@users.noreply.github.com>
2024-07-15 10:43:22 +0900

    Update 이진트리생성.kt

--- programmers/이진트리생성.kt
+++ programmers/이진트리생성.kt
@@ -15,12 +15,16 @@ class Solution {
         val nodeMap: MutableMap<Int, TreeNode> = mutableMapOf()
 
         // descriptions 배열을 순회하며 TreeNode 객체를 생성하고 맵에 추가
+        // 각 노드의 값(데이터)을 기준으로 TreeNode 객체를 생성하고 맵에 저장합니다.
+        // 이렇게 하면 나중에 부모-자식 관계를 설정할 때 쉽게 접근할 수 있습니다.
         for (description in descriptions) {
             nodeMap.getOrPut(description[0]) { TreeNode(description[0]) }
             nodeMap.getOrPut(description[1]) { TreeNode(description[1]) }
         }
 
         // descriptions 배열을 순회하며 TreeNode 객체의 left, right 포인터 설정
+        // 각 description에 대해 부모 노드와 자식 노드를 찾아 left 또는 right 포인터를 설정합니다.
+        // isLeft 값에 따라 왼쪽 자식 노드 또는 오른쪽 자식 노드로 설정합니다.
         for (description in descriptions) {
             val parent = nodeMap[description[0]]!!
             val child = nodeMap[description[1]]!!
@@ -32,6 +36,7 @@ class Solution {
         }
 
         // 루트 노드 찾기
+        // 모든 노드의 키 집합에서 자식 노드들의 키를 제거하면 남은 키가 루트 노드의 키입니다.
         val rootSet = nodeMap.keys.toMutableSet()
         for (description in descriptions) {
             rootSet.remove(description[1])
